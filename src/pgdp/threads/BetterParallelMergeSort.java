371640beb242d23f62f8f1928ba8c408fb791af6
kiu_medea.shvangiradze <medea.shvangiradze@kiu.edu.ge>
2021-12-24 20:07:54 +0400

    no message

---
+++ src/pgdp/threads/BetterParallelMergeSort.java
@@ -0,0 +1,14 @@
+package pgdp.threads;
+
+import java.util.concurrent.RecursiveAction;
+
+public class BetterParallelMergeSort extends RecursiveAction {
+    public BetterParallelMergeSort(Comparable[] array) {
+        // TODO
+    }
+    
+    @Override
+    protected void compute() {
+        // TODO
+    }
+}
---
+++ src/pgdp/threads/ParallelMergeSort.java
@@ -0,0 +1,14 @@
+package pgdp.threads;
+
+import java.util.concurrent.RecursiveAction;
+
+public class ParallelMergeSort extends RecursiveAction {
+    public ParallelMergeSort(Comparable[] array) {
+        // TODO
+    }
+
+    @Override
+    protected void compute() {
+        // TODO
+    }
+}
